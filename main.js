215265bb0c1c8ff46fc778477e2b1a2a26427197
Sbaayel <baayel@gmail.com>
2020-03-12 10:30:12 -0400

    Project1

---
+++ main.js
@@ -0,0 +1,194 @@
+// Axios calls will need to be formatted like this:
+// axios.get("your endpoint url",
+//     {
+//       "x-api-key":
+//     })
+// const apikey = "916f1bb3-859b-4408-987c-416fce877f19";
+// let endpoint = "https://api.thecatapi.com/v1/categories";
+// const button = document.querySelector("#try-me");
+// const select = document.querySelector("select");
+// // ---------------------------------------------
+// // Select Drop down
+// //----------------------------------------------
+// const categories = async () => {
+//   const response = await axios.get(`${endpoint}`, {
+//     "x-api-key": apikey
+//   });
+//   response.data.forEach(category => {
+//     select.innerHTML += `<option value = ${category.id}> ${category.name} </option>`;
+//   });
+//   console.log(response);
+// };
+// categories();
+// //-----------------------------------------
+// // images
+// // ----------------------------------------
+// button.addEventListener("click", async () => {
+//   const resp = await axios.get(
+//     `https://api.thecatapi.com/v1/images/search?category_ids=${select.value}`,
+//     {
+//       "x-api-key": apikey
+//     }
+//   );
+//   const catPic = document.querySelector("#catpic");
+//   catPic.innerHTML = `<img src = "${resp.data[0].url}" />`;
+// });
+
+// npm i -g surge
+
+// password = nasal@123
+
+// let $selectBox1 = $("select#select-box1");
+// $selectBox1.change(function() {
+//   let id = $(this)
+//     .children(":selected")
+//     .attr("id");
+//   getDogByBreed(id);
+// });
+
+async function test() {
+  let resp = await axios.get("https://api.thedogapi.com/v1/breeds");
+  console.log(resp.data);
+}
+test();
+
+let endpoint = document - querySelector("https://api.thedogapi.com/v1/breeds");
+const select = document.querySelector("select-box1");
+const option = document.querySelector("select");
+// ---------------------------------------------
+// Select Drop down
+//----------------------------------------------
+const categories = async () => {
+  const response = await axios.get(`${endpoint}`);
+  response.data.forEach(category => {
+    select.innerHTML += `<option value = ${category.id}> ${category.name} </option>`;
+  });
+  console.log(response);
+};
+categories();
+//-----------------------------------------
+// images
+// ----------------------------------------
+select.addEventListener("click", async () => {
+  const resp = await axios.get(
+    `https://api.thedogapi.com/v1/images/search?include_breed=1&breed_id=`
+  );
+  const breedImage = document.querySelector("#breed-image");
+  breedImage.innerHTML = `<img src = "${resp.data[0].url}" />`;
+});
+
+// function getBreeds() {
+//   ajax_get("https://api.thedogapi.com/v1/breeds", function(data) {
+//     populateBreedsSelect(data);
+//   });
+// }
+// function populateBreedsSelect(breeds) {
+//   $selectBox1.empty().append(function() {
+//     let output = "";
+//     $.each(breeds, function(key, value) {
+//       output += '<option id="' + value.id + '">' + value.name + "</option";
+//     });
+//     return output;
+//   });
+// }
+
+// function getDogByBreed(breed_id) {
+//   ajax_get(
+//     "https://api.thedogapi.com/v1/images/search?include_breed=1&breed_id=" +
+//       breed_id,
+//     function(data) {
+//       if (data.lenght == 0) {
+//         clearBreed();
+//         $("#breed-data-table").append(
+//           "<tr><td>Sorry, no image for that breed yet</td></tr>"
+//         );
+//       } else {
+//         displayBreed(data[0]);
+//       }
+//     }
+//   );
+// }
+
+// function clearBreed() {
+//   $("#breed-image").attr("src", "");
+//   $("#breed-data-table tr").remove();
+// }
+
+// function displayBreed(image) {
+//   $("#breed-image").attr("src", image.url);
+//   $("#breed-data-table tr").remove();
+//   let breedData = image.breeds[0];
+//   $.each(breed - data, function(key, value) {
+//     if (key == "weight" || key == "height") value = value.metric;
+//     $("#breed-data-table").append(
+//       "<tr><td>" + key + "</td><td>" + value + "</td></tr>"
+//     );
+//   });
+// }
+
+// function ajax_get(url, callback) {
+//   var xmlhttp = new XMLHttpRequest();
+//   xmlhttp.onreadystatechange = function() {
+//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//       console.log("responseText:" + xmlhttp.responseText);
+//       try {
+//         var data = JSON.parse(xmlhttp.responseText);
+//       } catch (err) {
+//         console.log(err.message + " in " + xmlhttp.responseText);
+//         return;
+//       }
+//       callback(data);
+//     }
+//   };
+//   xmlhttp.open("GET", url, true);
+//   xmlhttp.send();
+// }
+// getBreeds();
+
+// const coll = document.getElementsByClassName("collapsible");
+// const i;
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//    let content = this.nextElementSibling;
+//     if (content.style.maxHeight) {
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   });
+// }
+
+// function openForm() {
+//   document.getElementById("popupForm").style.display = "block";
+// }
+
+// function closeForm() {
+//   document.getElementById("popupForm").style.display = "none";
+// }
+
+// $("select").on("click" , function() {
+
+//   $(this).parent(".select-box").toggleClass("open");
+
+// });
+
+// $(document).mouseup(function (e)
+// {
+//     var container = $(".select-box");
+
+//     if (container.has(e.target).length === 0)
+//     {
+//         container.removeClass("open");
+//     }
+// });
+
+// $("select").on("change" , function() {
+
+//   var selection = $(this).find("option:selected").text(),
+//       labelFor = $(this).attr("id"),
+//       label = $("[for='" + labelFor + "']");
+
+//   label.find(".label-desc").html(selection);
+
+// });
---
+++ upload.js
@@ -0,0 +1,44 @@
+// const myToken =
+//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJoRHp3UVNjUnlKQVpiSmFSYTBIQWU5QnlxeXF5akJyaVlJNEJ3eXFmRmRZWmhGT0h1OCIsImp0aSI6ImIxOGU0YmI4NGIzMmViY2ZhNjNmYTk0MmM0YmJiMGQ4YjM1YzY0NDkyYzc4MWNjMjRiNmNjZGJlMzM3NGJiZTAyMjk5MzA1YzdlMTZjMjE4IiwiaWF0IjoxNTgzODU2MTc3LCJuYmYiOjE1ODM4NTYxNzcsImV4cCI6MTU4Mzg1OTc3Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.TIJL4ER-BL66XgkWHgBBFJcRUHuoEqi5VjqrHfHfrfUxHqMwVn5BiGavGLgvbBYJp7ac1wNClMhrsEoAeo-kPHmanseenB45aakJgF0cK-jACJCtNd2Vjx3ZktkfJ_LCBCAlRK-_bX0947XqkhttEE9HFfUq3YugVw-tT6AwQ__0x8sujWvw6pPDEroTcsN6Kpi1olq_h6GhAqMi5nqr1xK3CqlbgK-HhlGyUpWxDcnMImw7ewzO0EwlQ_TcubEYwkQ0QaAxO2LM899u1oFKYxi5rvAW0J5HDYaJG9CncpiZdsW4IXsGaV-GmuOMpbLgiTfL_9ZUg5tjXQ4UTDAjyw";
+// const requestHeaders = { Authorization: `Bearer ${myToken}` };
+
+const button = document.querySelector("#breed-btn");
+const select = document.querySelector("#my-select");
+const imageDiv = document.querySelector("#image-div");
+button.addEventListener("click", async () => {
+  event.preventDefault();
+  let breed = select.value.dog;
+  let response = await axios.get(`https://api.thedogapi.com/v1/${breed}breeds`);
+
+  console.log(response);
+  debugger;
+  let dogPic = response.data.message;
+  imageDiv.innerHTML = `<img src=${dogPic}>`;
+});
+
+// const breed = (url, callback) => {
+//   const request = new XMLHttpRequest();
+//   request.onreadystatechange = function() {
+//     if (request.readyState == 4 && request.status == 200) {
+//       console.log("responseText:" + request.responseText);
+//       try {
+//         let data = JSON.parse(request.responseText);
+//       } catch (err) {
+//         console.log(err.message + " in " + request.responseText);
+//         return;
+//       }
+//       callback(data);
+//     }
+//   };
+
+//   request.open("GET", url, true);
+//   request.send();
+// };
+
+// breed("https://api.thedogapi.com/v1/breeds", function(data) {
+//   document.getElementById("id").innerHTML = data[0]["id"];
+//   document.getElementById("url").innerHTML = data[0]["url"];
+
+//   let html = '<img src="' + data[0]["url"] + '">';
+//   document.getElementById("image").innerHTML = html;
+// });
